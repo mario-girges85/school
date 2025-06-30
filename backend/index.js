0ec1a7ae42447eac4932983507a4b206dc453c73
mario <mario85.girges@gmail.com>
2025-06-30 19:59:53 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://school-bay-nine.vercel.app",
-      "https://school-6i7h.vercel.app",
+      "http://localhost:5173/",
+      "https://school-bay-nine.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
